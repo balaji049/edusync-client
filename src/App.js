9f2dd47b42a5d22ad90af592338fa3e31a079ceb
balaji049 <049balaji@gmail.com>
2026-02-11 20:01:17 +0530

    index

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
+// src/App.js
 import React from "react";
 import "./App.css";
 import MainLayout from "./layouts/MainLayout";
 import AppRoutes from "./routes/AppRoutes";
 import { CallProvider } from "./context/CallContext";
-import Navbar from "./components/Navbar"; // 🔥 MISSING IMPORT FIXED
 
 function App() {
   return (
     <CallProvider>
-      <Navbar />          {/* 🔥 MUST BE HERE */}
       <MainLayout>
         <AppRoutes />
       </MainLayout>
