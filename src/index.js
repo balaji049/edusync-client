08f7f756624f85e52ce83ba38e0d3abcb7443078
balaji049 <049balaji@gmail.com>
2026-02-11 19:51:42 +0530

    asdf

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import AuthProvider from "./context/AuthContext";
-const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
 
 root.render(
   <BrowserRouter>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </BrowserRouter>
 );
